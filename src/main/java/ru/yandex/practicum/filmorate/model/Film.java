908bd0bca7bd5168b1324a3c88b08b101e1aabd0
Andrei Zabrodin <za8rodin.andr@yandex.ru>
2026-03-22 21:00:23 +0300

    Добавить возможность добавлять в друзья и ставить лайки

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,19 +1,26 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.validation.constraints.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.extern.slf4j.Slf4j;
 import ru.yandex.practicum.filmorate.validator.ValidReleaseDate;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Slf4j
 public class Film {
 
     private static final int MAX_DESCRIPTION = 200;
-
     private int id;
+
+    @JsonIgnore
+    @EqualsAndHashCode.Exclude
+    private Set<User> thoseWhoLiked = new HashSet<>();
 
     @NotBlank(message = "У фильма должно быть заполнено название")
     private String name;
@@ -26,4 +33,8 @@ public class Film {
 
     @Positive(message = "Продолжительность фильма должна быть больше 0!")
     private Integer duration;
+
+    public static int getLikesAmount(Film film) {
+        return film.getThoseWhoLiked().size();
+    }
 }
