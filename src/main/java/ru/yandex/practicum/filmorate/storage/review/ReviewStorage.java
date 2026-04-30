4649c2e7bf7a1d0f1e62cae043677571ac536ce7
Ivan Volkov <ivolkov03@mail.ru>
2026-04-30 19:10:58 +0300

    fix: Исправлены ошибки по стилю

--- src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/review/ReviewStorage.java
@@ -6,11 +6,18 @@ import java.util.Optional;
 
 public interface ReviewStorage {
     Review addReview(Review review);
+
     Review updateReview(Review review);
+
     void deleteReview(int reviewId);
+
     Optional<Review> getReviewById(int reviewId);
+
     Collection<Review> getReviewsByFilmId(int filmId, int count);
+
     void addReaction(int reviewId, int userId, boolean isLike);
+
     void removeReaction(int reviewId, int userId);
+    
     void updateUsefulRating(int reviewId);
 }
